3a674af376153c0f51011922e91e30fc90fcb163
Shivani Kumari <shivijha1603@gmail.com>
2024-09-23 19:06:26 +0530

    clean up

--- src/main/java/com/eureka/EurekaServer.java
+++ src/main/java/com/eureka/EurekaServer.java
@@ -3,13 +3,19 @@ package com.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServer.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+    
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
